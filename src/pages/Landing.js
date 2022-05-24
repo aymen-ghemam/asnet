0416cff0737dd2d50cd600ca855af9ad25d90aca
Hamaidi Youcef <youcefislam534@gmail.com>
2022-05-24 19:43:56 +0100

    Aricles page modification v1.1

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "../components/Button";
 import Carousel from "../components/Carousel";
-import SimpleCard from "../components/SimpleCard";
 
 const Landing = () => {
   const list = [
