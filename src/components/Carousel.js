7d80a184839f70978c5c9f85ac433b31ae6b19c7
Hamaidi Youcef <youcefislam534@gmail.com>
2022-05-24 15:12:31 +0100

    Done with landing page v1.1

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,82 +1,123 @@
-import React, { useState } from 'react'
-import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
-import Button from './Button'
-import './Carousel.css'
+import React, { useState } from "react";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import Button from "./Button";
+import "./Carousel.css";
 
 const Carousel = (props) => {
-  const [state, setState] = useState({list: props.list, visible: props.list[0], index: 0});
+  const [state, setState] = useState({
+    list: props.list,
+    visible: props.list[0],
+    index: 0,
+  });
   // const [fade, setFade] = useState(false);
 
   const prev = () => {
-    let index = state.index === 0? state.list.length-1: state.index-1;
-    setState({...state, visible: state.list[index], index});
-  }
+    let index = state.index === 0 ? state.list.length - 1 : state.index - 1;
+    setState({ ...state, visible: state.list[index], index });
+  };
   const next = () => {
-    let index = state.index === state.list.length-1? 0: state.index+1;
-    setState({...state, visible: state.list[index], index});
-  }
-  
+    let index = state.index === state.list.length - 1 ? 0 : state.index + 1;
+    setState({ ...state, visible: state.list[index], index });
+  };
+
   return (
-    <div className='carousel-container'>
-        <div className={`carousel flex ${state.index === 0? 'fade-in': 'fade-out hidden'}`}>
-          <div className='image'>
-              <div className='img'><img src={state.visible.img} alt="image" /></div>
+    <div className="carousel-container">
+      <AiOutlineLeft className="controler" onClick={prev} />
+      <div className="EventContainer">
+        <div
+          className={`carousel ${
+            state.index === 0 ? "fade-in" : "fade-out hidden"
+          }`}
+        >
+          <div className="carouselImage">
+            <div className="img">
+              <img src={state.visible.img} alt="image" />
+            </div>
           </div>
-          <div className='desc flex'>
-              <h3>{state.visible.title}</h3>
-              <p>{state.visible.description}</p>
-              <div><Button link={state.visible.link} text='Lire la suite.' /></div>
-          </div>
-        </div>
-
-        <div className={`carousel flex ${state.index === 1? 'fade-in': 'fade-out hidden'}`}>
-          <div className='image'>
-              <div className='img'><img src={state.visible.img} alt="image" /></div>
-          </div>
-          <div className='desc flex'>
-              <h3>{state.visible.title}</h3>
-              <p>{state.visible.description}</p>
-              <div><Button link={state.visible.link} text='Lire la suite.' /></div>
-          </div>
-        </div>
-
-        <div className={`carousel flex ${state.index === 2? 'fade-in': 'fade-out hidden'}`}>
-          <div className='image'>
-              <div className='img'><img src={state.visible.img} alt="image" /></div>
-          </div>
-          <div className='desc flex'>
-              <h3>{state.visible.title}</h3>
-              <p>{state.visible.description}</p>
-              <div><Button link={state.visible.link} text='Lire la suite.' /></div>
+          <div className="carouselDesc">
+            <h3>{state.visible.title}</h3>
+            <p>{state.visible.description}</p>
+            <div>
+              <Button link={state.visible.link} text="Lire la suite." />
+            </div>
           </div>
         </div>
-
-        <div className={`carousel flex ${state.index === 3? 'fade-in': 'fade-out hidden'}`}>
-          <div className='image'>
-              <div className='img'><img src={state.visible.img} alt="image" /></div>
+        <div
+          className={`carousel ${
+            state.index === 1 ? "fade-in" : "fade-out hidden"
+          }`}
+        >
+          <div className="carouselImage">
+            <div className="img">
+              <img src={state.visible.img} alt="image" />
+            </div>
           </div>
-          <div className='desc flex'>
-              <h3>{state.visible.title}</h3>
-              <p>{state.visible.description}</p>
-              <div><Button link={state.visible.link} text='Lire la suite.' /></div>
-          </div>
-        </div>
-
-        <div className={`carousel flex ${state.index === 4? 'fade-in': 'fade-out hidden'}`}>
-          <div className='image'>
-              <div className='img'><img src={state.visible.img} alt="image" /></div>
-          </div>
-          <div className='desc flex'>
-              <h3>{state.visible.title}</h3>
-              <p>{state.visible.description}</p>
-              <div><Button link={state.visible.link} text='Lire la suite.' /></div>
+          <div className="carouselDesc">
+            <h3>{state.visible.title}</h3>
+            <p>{state.visible.description}</p>
+            <div>
+              <Button link={state.visible.link} text="Lire la suite." />
+            </div>
           </div>
         </div>
-        
-        <AiOutlineLeft className='control left' onClick={prev} />
-        <AiOutlineRight className='control right' onClick={next} />
+        <div
+          className={`carousel ${
+            state.index === 2 ? "fade-in" : "fade-out hidden"
+          }`}
+        >
+          <div className="carouselImage">
+            <div className="img">
+              <img src={state.visible.img} alt="image" />
+            </div>
+          </div>
+          <div className="carouselDesc">
+            <h3>{state.visible.title}</h3>
+            <p>{state.visible.description}</p>
+            <div>
+              <Button link={state.visible.link} text="Lire la suite." />
+            </div>
+          </div>
+        </div>
+        <div
+          className={`carousel ${
+            state.index === 3 ? "fade-in" : "fade-out hidden"
+          }`}
+        >
+          <div className="carouselImage">
+            <div className="img">
+              <img src={state.visible.img} alt="image" />
+            </div>
+          </div>
+          <div className="carouselDesc">
+            <h3>{state.visible.title}</h3>
+            <p>{state.visible.description}</p>
+            <div>
+              <Button link={state.visible.link} text="Lire la suite." />
+            </div>
+          </div>
+        </div>
+        <div
+          className={`carousel ${
+            state.index === 4 ? "fade-in" : "fade-out hidden"
+          }`}
+        >
+          <div className="carouselImage">
+            <div className="img">
+              <img src={state.visible.img} alt="image" />
+            </div>
+          </div>
+          <div className="carouselDesc">
+            <h3>{state.visible.title}</h3>
+            <p>{state.visible.description}</p>
+            <div>
+              <Button link={state.visible.link} text="Lire la suite." />
+            </div>
+          </div>
+        </div>
+      </div>
+      <AiOutlineRight className="controler" onClick={next} />
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,71 +1,108 @@
-import React from 'react'
-import Button from '../components/Button'
-import Carousel from '../components/Carousel'
-import SimpleCard from '../components/SimpleCard'
+import React from "react";
+import Button from "../components/Button";
+import Carousel from "../components/Carousel";
+import SimpleCard from "../components/SimpleCard";
 
 const Landing = () => {
   const list = [
     {
-      title: 'Event 1',
-      description: 'Lorem ipsum dolor sit amet consectetur .',
-      img: '/assets/Ramadan-2022.webp',
-      link: '#'
+      title: "Event 1",
+      description: "Lorem ipsum dolor sit amet consectetur .",
+      img: "/assets/Ramadan-2022.webp",
+      link: "#",
     },
     {
-      title: 'something ',
-      description: 'something ..something ..something ..something ..something ..something ..something ...',
-      img: '/assets/Ramadan-2022.webp',
-      link: '#'
+      title: "something ",
+      description:
+        "something ..something ..something ..something ..something ..something ..something ...",
+      img: "/assets/Ramadan-2022.webp",
+      link: "#",
     },
     {
-      title: 'Ramadan',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, consequatur aperiam! Qui nam odit optio. Amet itaque, possimus iure modi quisquam .',
-      img: '/assets/Ramadan-2022.webp',
-      link: '#'
+      title: "Ramadan",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, consequatur aperiam! Qui nam odit optio. Amet itaque, possimus iure modi quisquam .",
+      img: "/assets/Ramadan-2022.webp",
+      link: "#",
     },
     {
-      title: 'Ramadan',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, consequatur aperiam! Qui nam odit optio. Amet itaque, possimus iure modi quisquam .',
-      img: '/assets/Ramadan-2022.webp',
-      link: '#'
+      title: "Ramadan",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, consequatur aperiam! Qui nam odit optio. Amet itaque, possimus iure modi quisquam .",
+      img: "/assets/Ramadan-2022.webp",
+      link: "#",
     },
     {
-      title: '2022',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, consequatur aperiam! Qui nam odit optio. Amet itaque, possimus iure modi quisquam .',
-      img: '/assets/Ramadan-2022.webp',
-      link: '#'
-    }
-  ]
+      title: "2022",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, consequatur aperiam! Qui nam odit optio. Amet itaque, possimus iure modi quisquam .",
+      img: "/assets/Ramadan-2022.webp",
+      link: "#",
+    },
+  ];
   return (
     <div>
-      <section className='landing flex row'>
-        <div className='text flex'>
-            <h1>Parce qu'un enfant en situation de handicap est avant tout un enfant</h1>
-            <p>L'ASNET offre un service d'apprentissage et de prise en charge personnalisé.</p>
-            <Button link='#' text='Soutenire notre action' />
+      <section className="headerContainer">
+        <div className="headerImage">
+          <img src="./assets/enfant_qui_lit.svg" alt="" />
         </div>
-        <div className='svg flex'>
-            <img src='./assets/enfant_qui_lit.svg' alt='' />
+        <div className="headerDesc">
+          <h1>
+            Parce qu'un <span className="OrangeText">enfant</span> en situation
+            de handicap est avant tout un enfant
+          </h1>
+          <p>
+            L'ASNET offre un service d'apprentissage et de prise en charge
+            personnalisé.
+          </p>
+          {/* <Button link='#' text='Soutenire notre action' /> */}
         </div>
       </section>
-      <section className='about'>
-        <p className='text'>L'ASNET s’engage depuis plus de 3 ans pour faire émerger une société inclusive dès le plus jeune âge </p>
-        <div className='cards flex'>
-          <SimpleCard />
-          <SimpleCard />
-          <SimpleCard />
+      <section className="about">
+        <div className="aboutMain">
+          <div className="aboutText">
+            <p>
+              L'<span className="OrangeText">ASNET</span> s’engage depuis plus
+              de 3 ans pour faire émerger une société inclusive dès le plus
+              jeune âge{" "}
+            </p>
+          </div>
+          <div className="aboutCardContainer">
+            <div className="aboutCardDesc">
+              <p>
+                Accompagner et soutenir les familles à chaque étape de leurs
+                parcours de vie
+              </p>
+            </div>
+          </div>
+          <div className="aboutCardContainer card2">
+            <div className="aboutCardDesc aboutCardDesc2">
+              <p>
+                Favoriser l’accueil des enfants en situation de handicap dans le
+                milieu ordinaire
+              </p>
+            </div>
+          </div>
+          <div className="aboutCardContainer card3">
+            <div className="aboutCardDesc aboutCardDesc3">
+              <p>
+                Former tous les acteurs concernés par la situation de handicap
+                de l’enfant vers une dynamique inclusive
+              </p>
+            </div>
+          </div>
         </div>
-        
       </section>
-      <section className='latest'>
-        <h2 className='title'>Nos dernières actualités</h2>
-        <Carousel 
-          list = {list}
-        />
+      <section className="latest">
+        <div className="carouselHeader">
+          <h2 className="title">Nos dernières actualités</h2>
+        </div>
+        <div className="carouselContainer">
+          <Carousel list={list} />
+        </div>
       </section>
     </div>
+  );
+};
 
-  )
-}
-
-export default Landing
+export default Landing;
