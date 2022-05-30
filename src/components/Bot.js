3ee7caf879416c9092d2cd9c60c220844412c235
aymen-ghemam <gh.aymen39@gmail.com>
2022-05-30 19:29:45 +0100

    add chat bot

---
+++ src/components/Bot.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { AiFillPropertySafety } from "react-icons/ai";
+import {BsChatRightTextFill} from 'react-icons/bs'
+import { IoMdSend } from "react-icons/io";
+import Message from "../components/Message";
+import './Bot.css'
+
+
+const Bot = (props) => {
+    const [state, setstate] = useState({
+      msg: "",
+      request: "",
+      list: [],
+    });
+    const [expanded, setExpanded] = useState(false);
+  
+    const toggle = () => {
+      setExpanded(!expanded);
+    };
+  
+    const handleChange = (e) => {
+      setstate({ ...state, msg: e.target.value });
+    };
+  
+    const handleKeyPress = (e) => {
+      if (e.code === "Enter") {
+        handleSend();
+      }
+    };
+    const sleep = (ms) => {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    };
+  
+    const handleSend = () => {
+      if (state.msg) {
+        const msg = state.msg;
+        let list = state.list;
+        list.push({ text: msg, type: "right" });
+        setstate({ ...state, list, request: msg });
+        document.getElementById("msg").value = "";
+      }
+    };
+  
+    // useEffect(async () => {
+    //   if (state.request) {
+    //     let msg = "Unknown command!";
+    //     if (state.request.search("exam") !== -1)
+    //       msg = "First semester exams will be starting on January 16th";
+    //     if (state.request.search("emergen") !== -1) {
+    //       msg = "Report an emergency!";
+    //       await sleep(2000);
+    //       props.setCurrent("report");
+    //     }
+  
+    //     await sleep(1000);
+    //     setstate({
+    //       ...state,
+    //       list: [...state.list, { text: msg, type: "left" }],
+    //     });
+    //   }
+    // }, [state.request]);
+  
+    return expanded ? (
+        <div id="bot" onKeyPress={handleKeyPress}>
+          <div className="head" onClick={toggle}>
+            <h3>Chat Bot</h3>
+          </div>
+          <div className="body">
+            {state.list.map((msg, index) => (
+              <Message text={msg.text} type={msg.type} />
+            ))}
+          </div>
+          <div className="bottom">
+            <input
+              id="msg"
+              type="text"
+              placeholder="ask something ..."
+              onChange={handleChange}
+            />
+            <IoMdSend color="#118BF1" size="1.7em" onClick={handleSend} />
+          </div>
+        </div>
+      ) : (
+        <div className="min-bot" onClick={toggle}>
+          <span><BsChatRightTextFill className='bot-icon' size='2em' color='#fff' /> </span>
+        </div>
+      );
+  };
+  
+  export default Bot;
